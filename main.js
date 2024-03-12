92ed9dc8a7b32c82f4a886d8cc33a0c56c8ebb36
ismailxonov <ismailxonovsiratulloh@gmail.com>
2024-03-12 09:13:35 +0500

    Hello Mars

---
+++ main.js
@@ -0,0 +1,11 @@
+let name = prompt("What your name?")
+console.log("Name =" + name);
+let surname = prompt("What your surname?")
+console.log("Surname =" + surname);
+let age = prompt("What your age?")
+console.log("Age =" + age);
+let subject = prompt("Enter your favorite subject?")
+console.log("Subject =" + subject);
+let fro = prompt("Enter your county of residence")
+console.log("From =" + fro)
+alert("Thank you for entering your information" )
